284a724d7ce14b3e560eb55144678932ffe58184
전상은 <wowddok99@gmail.com>
2024-11-27 01:47:26 +0900

    refactor(v1.4): 불필요 코드 제거

--- src/main/java/tour_recommend/tour_recommend_back/repository/SalePostRepository.java
+++ src/main/java/tour_recommend/tour_recommend_back/repository/SalePostRepository.java
@@ -8,7 +8,5 @@ import tour_recommend.tour_recommend_back.entity.SalePost;
 import java.util.List;
 
 public interface SalePostRepository extends JpaRepository<SalePost, Long> {
-    // 카테고리별 후기 조회 - createdAt 기준 최신순 반환
-    List<SalePost> findAllByCategoryOrderByCreatedAtDesc(String category);
     Page<SalePost> findByCategory(String category, PageRequest pageRequest);
 }
